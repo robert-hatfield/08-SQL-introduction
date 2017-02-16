8403d1484b123e44775082249c2ef2c6b82445f6
robert-hatfield <robert-hatfield@users.noreply.github.com>
2017-02-15 16:20:30 -0800

    Complete TODOs covered during lecture

--- starter-code/server.js
+++ starter-code/server.js
@@ -1,16 +1,16 @@
 'use strict';
-// TODO: Install and require the node postgres package into your server.js, and ensure that it's now a new dependency in your package.json
-// const pg = require('pg');
+// DONE: Install and require the Node postgres package 'pg' into your server.js, and ensure that it's now a new dependency in your package.json
+const pg = require('pg');
 const express = require('express');
 // REVIEW: Require in body-parser for post requests in our server
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 const app = express();
-// TODO: Complete the connection string for the url that will connect to your local postgres database
+// DONE: Complete the connection string for the url that will connect to your local postgres database
 // Windows and Linux users; You should have retained the user/pw from the pre-work for this course.
 // Your url may require that it's composed of additional information including user and password7
 // NOTE: Students will have varying URLs depending on their OS
-// const conString = 'postgres://localhost:5432';
+const conString = 'postgres://localhost:5432';
 // REVIEW: Pass the conString to pg, which creates a new client object
 const client = new pg.Client(conString);
 // REVIEW: Use the client object to connect to our DB.
